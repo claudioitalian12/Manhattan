cb5913d589eb869ea04b1a397724b04423ab09ce
Claudio Cavalli <claudio.cavalli@vodafone.com>
2022-11-25 01:01:34 +0100

    Initial Commit

---
+++ Manhattan/ManhattanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManhattanApp.swift
+//  Manhattan
+//
+//  Created by Claudio Cavalli on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ManhattanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
